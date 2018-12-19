364479869680a23f50417e1c2b172374d8d72215
Miguel Boland <migueldboland@gmail.com>
2018-12-19 10:13:42 +0000

    Added last_updated field

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open(_requirements) as f:
 setup(
     name='emg_backlog_schema',
 
-    version='0.5.0',
+    version='0.5.1',
     description="Django DB schema for EBI-Metagenomics",
 
     author="Miguel Boland",
