2f51c7f245b89ca3e7ee8845946d91746a907889
Miguel Boland <migueldboland@gmail.com>
2019-02-25 16:32:47 +0000

    Renamed cram field to bam

--- backlog/models.py
+++ backlog/models.py
@@ -189,7 +189,7 @@ class AssemblyJob(models.Model):
                                                 null=True)
 
     uploaded_to_ena = models.NullBooleanField()
-    cram_uploaded = models.NullBooleanField()
+    bam_uploaded = models.NullBooleanField()
     new_ena_assembly = models.CharField(max_length=20, null=True)
     runs = models.ManyToManyField(Run, through='RunAssemblyJob', related_name='assemblyjobs', blank=True)
 
