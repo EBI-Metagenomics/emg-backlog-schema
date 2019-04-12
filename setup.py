bf60a828d1929d8c35c9cc757703ee40f735caf7
Miguel Boland <migueldboland@gmail.com>
2019-04-12 10:19:46 +0100

    Removed un-needed id column from biome database, biome_id is now used as primary key

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open(_requirements) as f:
 setup(
     name='emg_backlog_schema',
 
-    version='0.8.2',
+    version='0.9.0',
     description="Django DB schema for EBI-Metagenomics",
 
     author="Miguel Boland",
