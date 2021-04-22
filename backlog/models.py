bb2d471b490fa7a9cee85d5cabf47972187f57ca
Ekaterina Sakharova <kates@ebi.ac.uk>
2021-04-22 16:09:49 +0100

    change desc for field

--- backlog/models.py
+++ backlog/models.py
@@ -317,5 +317,5 @@ class AssemblyAnnotationJob(models.Model):
     assembly = models.ForeignKey(Assembly, on_delete=models.DO_NOTHING, related_name='assemblyannotationjobs')
     annotation_job = models.ForeignKey(AnnotationJob, on_delete=models.CASCADE)
     protein_db = models.BooleanField(
-        "Boolean field to mark was assembly from Job added to protein DB or not",
+        "True if the linked assembly was added to the protein DB",
         default=False)
