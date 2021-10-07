70560d18765a5e914ba947140a4e0160fcc1f81a
Martín Beracochea <mbc@ebi.ac.uk>
2021-10-07 13:50:03 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open(_requirements) as f:
 setup(
     name='emg_backlog_schema',
 
-    version='1.0.0',
+    version='1.0.1',
     description="Django DB schema for EBI-Metagenomics",
 
     author="Miguel Boland",
